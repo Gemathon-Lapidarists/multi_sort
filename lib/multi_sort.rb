2a9179bc4c85ecea445161d806234ce52ef27a55
Sukhwinder S Tambar <sukhwinder.tambar@valuelabs.com>
2014-03-22 16:19:59 +0530

    [Feature] added sort by direction

--- lib/multi_sort.rb
+++ lib/multi_sort.rb
@@ -1,12 +1,12 @@
 require "multi_sort/version"
 
 module MultiSort
-  def self.sort_by_order hash = {}, order = []
+  def self.sort_by_order hash = {}, order = [], direction ='ASC'
     hash.sort do |a,b|
       a_arr = []
   	  b_arr = []
       order.each{|key| a_arr << a[key]; b_arr << b[key];}
-      a_arr <=> b_arr
+      direction == "DESC" ? b_arr <=> a_arr : a_arr <=> b_arr 
   	end
   end
 end
